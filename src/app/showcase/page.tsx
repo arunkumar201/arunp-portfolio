06100f10cd079c2fc3165a8993bec210b0f657cf
Arun Kumar <arunprajapati9825@gmail.com>
2023-11-25 12:23:49 +0530

    showcase added

--- src/app/showcase/page.tsx
+++ src/app/showcase/page.tsx
@@ -1,14 +1,33 @@
-import { Metadata } from 'next';
-import React from 'react'
+import { GITHUB_API_URL, GITHUB_USERNAME } from "../../../config";
 
-type Props = {}
+import { Metadata } from "next";
+import React from "react";
+import RepoCard from "@/utils/RepoCard";
+
 export const metadata: Metadata = {
 	title: "ShowCase | Arun Kumar",
 };
-const page = (props: Props) => {
-  return (
-	<div>page</div>
-  )
-}
+const page = async () => {
+	const res = await fetch(`${GITHUB_API_URL}/users/${GITHUB_USERNAME}/repos`, {
+		next: {
+			revalidate: 20000,
+		},
+	});
+	let allRepos = await res.json();
+	return (
+		<>
+			<section className="m-2">
+				<p className="text-2xl bg-gray-100 dark:bg-inherit">
+					Featured Repositories
+				</p>
+				<br />
+				<p className="w-[15.5rem] border-b-yellow-400 border-b-4 relative -top-7"></p>
+				<div className="mt-3 space-y-4">
+					<RepoCard repos={allRepos as GitHubRepository[]} />
+				</div>
+			</section>
+		</>
+	);
+};
 
-export default page
+export default page;
